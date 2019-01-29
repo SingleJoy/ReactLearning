e5482d8110aa0ca7db06ad89240fe78517e4cd62
SingleJoy <2511881919@qq.com>
2019-01-29 10:43:48 +0800

    初始化 react theme项目

---
+++ Theme/theme/src/view/Tab/Tab.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from '../Button/Button'
+
+class Tab extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+               <Button/>
+            </div>
+        )
+    }
+}
+
+
+export default Tab;
---
+++ Theme/theme/src/view/Theme/Theme.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Tab from '../Tab/Tab';
+
+
+class Theme extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+              <Tab/>
+            </div>
+        )
+    }
+}
+
+
+export default Theme;
